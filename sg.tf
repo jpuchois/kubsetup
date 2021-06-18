a0d13410de566687f43f09e684e472c0571fed80
Josselin Puchois <jpuchois@gmail.com>
2021-06-18 19:53:56 +0000

    split & rework file

---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "kub_sg" {
+  name        = "allow_all"
+  description = "Allow all"
+  vpc_id      = aws_vpc.kub_vpc.id
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "kub_SG"
+  }
+}
